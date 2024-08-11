f95c96cc761277237b754bd34cfdc105c31fab0f
egor5667 <80642256+egor5667@users.noreply.github.com>
2024-08-11 22:23:33 +0500

    11.08.2024

---
+++ app/handlers.py
@@ -0,0 +1,47 @@
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram import Router
+
+router = Router()
+
+
+class Reg(StatesGroup):
+    name = State()
+    Sname = State()
+
+
+
+@router.message(Command("help"))
+async def help_comand(message: Message):
+    await message.reply("Какой-то текст")
+
+
+
+
+@router.message(Command('start'))
+async def start_comand(message: Message, state: FSMContext):
+    await message.answer("Привет! Меня зовут Игрик. Я бот-помощник клуба настольных игр 'Играриум'. Давай знакомиться.")
+    await state.set_state(Reg.name)
+    await message.answer("Введите свое имя")
+
+@router.message(Reg.name)
+async def reg_s2(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.Sname)
+    await message.answer("Введите фамилию")
+
+@router.message(Reg.Sname)
+async def regFin(message: Message, state: FSMContext):
+    await state.update_data(Sname=message.text)
+    data = await state.get_data()
+    await message.answer(f"Регистрация завершена.Имя {data['name']}, фамилия {data['Sname']}")
+    await state.clear()
+
+
+
+
+@router.message(Command('hell'))
+async def hell_comand(message: Message):
+    await message.answer("ЭТО МОЁ БЛЯТЬ ДУШЕВНОЕ РАВНОВЕСИЕ!")
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
 from config import TOKEN_API
-from statesForm import StepForm
+from app.handlers import router
+
 
 
 bot = Bot(token=TOKEN_API)
@@ -16,44 +17,6 @@ dp = Dispatcher()
 
 
 
-class Reg(StatesGroup):
-    name = State()
-    Sname = State()
-
-
-
-@dp.message(Command("help"))
-async def help_comand(message: Message):
-    await message.reply("Какой-то текст")
-
-
-
-
-@dp.message(Command('start'))
-async def start_comand(message: Message, state: FSMContext):
-    await message.answer("Привет! Меня зовут Игрик. Я бот-помощник клуба настольных игр 'Играриум'. Давай знакомиться.")
-    await state.set_state(Reg.name)
-    await message.answer("Введите свое имя")
-
-@dp.message(Reg.name)
-async def reg_s2(message: Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await state.set_state(Reg.Sname)
-    await message.answer("Введите фамилию")
-
-@dp.message(Reg.Sname)
-async def regFin(message: Message, state: FSMContext):
-    await state.update_data(Sname=message.text)
-    data = await state.get_data()
-    await message.answer(f"Регистрация завершена.Имя {data['Name']}, фамилия {data['Sname']}")
-    await state.clear()
-
-
-
-
-@dp.message(Command('hell'))
-async def hell_comand(message: Message):
-    await message.answer("ЭТО МОЁ БЛЯТЬ ДУШЕВНОЕ РАВНОВЕСИЕ!")
 
 # @dp.message(Command("start"))
 # async def cmd_start(message: types.Message):
@@ -72,6 +35,7 @@ async def hell_comand(message: Message):
 # async def two(message: types.Message):
 #     await message.reply("ТОлько после тебя!")
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
