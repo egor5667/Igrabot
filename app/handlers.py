3ef70961242dc2d1fb5ebe65f6d1b9d6e7b18841
egor5667 <80642256+egor5667@users.noreply.github.com>
2024-08-13 22:09:12 +0500

    13.08.2024

--- app/handlers.py
+++ app/handlers.py
@@ -1,17 +1,31 @@
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram import Router
-import app.keyboards as kb
+from aiogram import Router, F
 
+
+import app.keyboards as kb
+from statuses import Reg
 
 router = Router()
 
+dfac = {
+    'et': 'Естественно-технологический факультет',
+    'sport': 'Высшая шк. физической культуры и спорта',
+    'ist': 'Исторический факультет',
+    'doshfak': 'Факультет дошкольного образования',
+    'fiko': 'Факультет инклюзивного и коррекц. образования',
+    'inyaz': 'Факультет иностранных языков',
+    'mfi': 'Факультет математики, физики и информатики',
+    'unk': 'Факультет подгот. учителей начальных классов',
+    'filfak': 'Филологический факультет',
+    'colege': 'Колледж',
+    'psifak': 'Факультет психологии',
+    'ppi': 'Профессионально-педагогический институт'
+}
 
-class Reg(StatesGroup):
-    name = State()
-    Sname = State()
+
 
 
 @router.message(Command("help"))
@@ -21,25 +35,28 @@ async def help_comand(message: Message):
 
 @router.message(Command('start'))
 async def start_comand(message: Message, state: FSMContext):
-    await message.answer("Привет! Меня зовут Игрик. Я бот-помощник клуба настольных игр 'Играриум'. Давай знакомиться.",
-                         reply_markup=kb.settings)
-    await state.set_state(Reg.name)
+    await message.answer("Привет! Меня зовут Игрик. Я бот-помощник клуба настольных игр 'Играриум'. Давай знакомиться.")
+    await state.set_state(Reg.qname)
     await message.answer("Введите свое имя")
 
+# @router.callback_query(F.data == 'catalog')
+# async def catal(calback: CallbackQuery):
+#     await calback.answer('')
+#     await calback.message.edit_text('Привет!', reply_markup=kb.settings)
 
-@router.message(Reg.name)
+@router.message(Reg.qname)
 async def reg_s2(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
-    await state.set_state(Reg.Sname)
+    await state.set_state(Reg.qsname)
     await message.answer("Введите фамилию")
 
 
-@router.message(Reg.Sname)
+@router.message(Reg.qsname)
 async def regFin(message: Message, state: FSMContext):
     await state.update_data(Sname=message.text)
-    data = await state.get_data()
-    await message.answer(f"Регистрация завершена.Имя {data['name']}, фамилия {data['Sname']}")
-    await state.clear()
+    await message.answer("Выберите свой факультет", reply_markup=kb.faculty_one)
+
+@router.callback_query(F.data == 'next')
 
 
 @router.message(Command('hell'))
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,14 +1,38 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='ОАОАОАОА')],
-    [KeyboardButton(text='ЧЕГООООООО?!'), KeyboardButton(text='ТОГОООО!')]
-], resize_keyboard=True, input_field_placeholder='Выберите пункт меню')
+# main = InlineKeyboardMarkup(inline_keyboard=[
+#     [InlineKeyboardButton(text='Каталог', callback_data='catalog')],
+#     [InlineKeyboardButton(text='Корзина', callback_data='basket'),
+#      InlineKeyboardButton(text='Контакты', callback_data='contact')]])
 
-settings = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='YT', url='https://youtube.com/@sudoteach')]
+# settings = InlineKeyboardMarkup(inline_keyboard=[
+#     [InlineKeyboardButton(text='YT', url='https://youtube.com/@sudoteach')]
+# ])
+
+
+faculty_one = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Естественно-технологический факультет', callback_data='et')],
+    [InlineKeyboardButton(text='Высшая шк. физической культуры и спорта', callback_data='sport')],
+    [InlineKeyboardButton(text='Исторический факультет', callback_data='ist')],
+    [InlineKeyboardButton(text='Факультет дошкольного образования', callback_data='doshfak')],
+    [InlineKeyboardButton(text=':right: Далее', callback_data='1to2')]
 ])
 
+faculty_two = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Факультет инклюзивного и коррекц. образования', callback_data='fiko')],
+    [InlineKeyboardButton(text='Факультет иностранных языков', callback_data='inyaz')],
+    [InlineKeyboardButton(text='Факультет математики, физики и информатики', callback_data='mfi')],
+    [InlineKeyboardButton(text='Факультет подгот. учителей начальных классов', callback_data='unk')],
+    [InlineKeyboardButton(text='след. страница', callback_data='2to3')],
+    [InlineKeyboardButton(text='Пред. страница', callback_data='2to1')]
+])
 
+faculty_three = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Филологический факультет', callback_data='filfak')],
+    [InlineKeyboardButton(text='Колледж', callback_data='colege')],
+    [InlineKeyboardButton(text='Факультет психологии', callback_data='psifak')],
+    [InlineKeyboardButton(text='Профессионально-педагогический институт', callback_data='ppi')],
+    [InlineKeyboardButton(text='Пред. страница', callback_data='3to2')]
+])
 
 
