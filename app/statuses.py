f0684b4c4113c1163781134b5cd0a64a2f9f11e7
egor5667 <80642256+egor5667@users.noreply.github.com>
2024-08-13 22:09:33 +0500

    аааа

---
+++ app/statuses.py
@@ -0,0 +1,12 @@
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
+
+class Reg(StatesGroup):
+    qname = State()
+    qsname = State()
+    qinstit = State()
+    fped = State()
+    noped = State()
+    qfaculty = State()
+    qcourse = State()
