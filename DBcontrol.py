a44ab79c1357ff728efd9f7df3e87097aa66b2d7
egor5667 <80642256+egor5667@users.noreply.github.com>
2024-08-16 23:31:17 +0500

    16.08.2024
    старт разрабу БД

--- DBcontrol.py
+++ DBcontrol.py
@@ -3,10 +3,24 @@ import sqlite3
 conn = sqlite3.connect('Igrarium.db')
 cur = conn.cursor()
 
-cur.execute('CREATE TALE if NOT EXISTS users (id int auto_increment primary key, name varchar(50), password varchar(50))')
+cur.execute(
+    'CREATE TABLE if NOT EXISTS users (id int auto_increment primary key,'
+    'name varchar(50),'
+    'sname varchar(50),'
+    'inst varchar(5),'
+    'faculty varchar(60),'
+    'course int)')
 conn.commit()
 cur.close()
-conn.close()
 
-async def sent_registData():
-    pass
+data = {'name': "fdfdsfdsf", 'sname': '32425425'}
+
+
+async def sent_registData(reg):
+    cur = conn.cursor()
+    cur.execute('INSERT INTO users (name, name, inst, faculty, course) VALUES (?, ?, ?, ?, ?)',
+                (reg['name'],
+                 reg['sname'], reg['inst'], reg['faculty'], reg['course']))
+    conn.commit()
+    cur.close()
+    conn.close()
