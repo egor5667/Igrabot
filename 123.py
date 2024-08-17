71638daf149c6c31199eb90997f96dfa55d43834
egor5667 <80642256+egor5667@users.noreply.github.com>
2024-08-17 23:18:29 +0500

    0000

---
+++ 123.py
@@ -0,0 +1,9 @@
+data = {'hsd':{'a': 'abobus', 'b': 'buble'}, 'hghg': {'r': 'reeder', 'g': 'giant'}}
+
+bab = {'k': 'KK', 'c': 'ggggg'}
+
+data['hsd'] = bab
+
+for i in data:
+    print(data[i])
+
--- main.py
+++ main.py
@@ -4,19 +4,17 @@ import logging
 from aiogram import Bot, Dispatcher
 import logging
 
-from config import TOKEN_API
+from testconf import TOKEN_API
 from app.handlers import router
-
-
 
 bot = Bot(token=TOKEN_API)
 dp = Dispatcher()
 
 
-
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
