48e84063e396a6039823bb3dcc6e9108225168a3
egor5667 <80642256+egor5667@users.noreply.github.com>
2024-09-02 18:38:51 +0500

    Create AchiveControl.py

---
+++ AchiveControl.py
@@ -0,0 +1,25 @@
+from aiogram.types import Message as message
+from DBcontrol import Achives
+from aiogram import Bot
+from testconf import TOKEN_API
+
+bot = Bot(token=TOKEN_API)
+
+class AchFReg:
+   def getRegAch(uid):
+      if not (Achives.SerchAchive(uid, 'reg')):
+         text = 'Поздравляю! Вы получили достижение "Welcome to the club..."'
+         Achives.AddAchive(uid, 'reg')
+         return text
+
+
+
+
+
+
+
+
+
+
+
+
